7ad744233a111cad9560763930cb46cb653f7256
Anjali Meena <Anjali.anjalimeena09052003@gmail.com>
2024-10-12 21:57:38 +0530

    Initial commit or your frontend message

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'; // Importing the CSS file in the same directory
+// import { Link } from 'react-router-dom';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+                <div className="footer-content">
+                    <span className="footer-text">Copyright© 2024-Anjali Meena, All right Reserved</span>
+                    <span className="footer-text">Contact No. +91-8306216084</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
